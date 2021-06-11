cc5407e64ea3731a755bac20a1676f33b34dc82f
wxxw12 <50473168+matiasvlevi@users.noreply.github.com>
2021-06-11 08:54:30 -0400

    map name value

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
-let path = './songs/FREEDOM_DiVE.osu'
+
+let name = 'FREEDOM_DiVE';
+let path = './songs/'+name+'.osu'
 const osujson = require('osu-json');
 
 const fs = require('fs');
@@ -7,9 +9,8 @@ let file = fs.readFileSync(path, 'utf8');
 
 osujson.ParseOSUFileAsync(file).then( (output)=>{
 	let data = JSON.stringify(output.HitObjects)
-	fs.writeFileSync('./compiled/FREEDOM_DiVE.json', data)
+	fs.writeFileSync('./compiled/'+name+'.json', data)
 })
 .catch( (err)=>{
     throw new Error(err);
 });
-// pp
