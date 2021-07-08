61544028f25459ffd5177328c02da195c5e4d1d2
Matias Vazquez-Levi <50473168+matiasvlevi@users.noreply.github.com>
2021-07-08 12:15:27 -0400

    handle osu not defined

--- webapp/sketch.js
+++ webapp/sketch.js
@@ -18,9 +18,10 @@ function preload() {
   });
     // /!\ Will remove http request once moved to local server /!\
 }
-
+let wnx = 511*2;
+let wny = 383*2;
 function setup() {
-  let canvas = createCanvas(511*2, 383*2);
+  let canvas = createCanvas(wnx, wny);
   canvas.parent('canvas');
   frameRate(60); 
 }
@@ -32,29 +33,39 @@ let speed = 5;
 
 
 // Time value
-let time = 1000;
+let time = 1500;
 function draw() {
-  scale(2)
-  background(51);    
-  if (time >= osu[osu.length-1].time - speed - 1) {
-    // If max time exceeded, restart at 0.
-    time = 0;
+  if (osu === undefined) {
+    background(0);
+    push();
+    textSize(32);
+    textAlign(CENTER);
+    fill(255)
+    text('osu map not found, please reload page', wnx/2, wny/2);
+    pop();
   } else {
-    // Increment time value
-    time+=speed;
-  }
-  // Scan every hitobject & display at right time
-  for (let i = 0; i < osu.length; i++) {
-    if (osu[i].time >= time && osu[i].time <= (time + delay)) {
-      let type = osu[i].type.type;
-      if (type === 'circle') {
-        hitObject = new Circle(osu[i].x, osu[i].y, osu[i].time);
-      } else if (type === 'slider') {
-        hitObject = new Slider(osu[i].x, osu[i].y, osu[i].path, osu[i].time);
-      } else {
-        console.log('Unhandled type: ' + type);
+    scale(2)
+    background(51);    
+    if (time >= osu[osu.length-1].time - speed - 1) {
+      // If max time exceeded, restart at 0.
+      time = 0;
+    } else {
+      // Increment time value
+      time+=speed;
+    }
+    // Scan every hitobject & display at right time
+    for (let i = 0; i < osu.length; i++) {
+      if (osu[i].time >= time && osu[i].time <= (time + delay)) {
+        let type = osu[i].type.type;
+        if (type === 'circle') {
+          hitObject = new Circle(osu[i].x, osu[i].y, osu[i].time);
+        } else if (type === 'slider') {
+          hitObject = new Slider(osu[i].x, osu[i].y, osu[i].path, osu[i].time);
+        } else {
+          console.log('Unhandled type: ' + type);
+        }
+        hitObject.render();
       }
-      hitObject.render();
     }
   }
 }
