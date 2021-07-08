7ee7551a83f9afe860faad06536d2d1c76fa2451
SharkAce <85720638+SharkAce@users.noreply.github.com>
2021-07-08 02:04:00 -0400

    name fix
    Changed the default .osu name so the example would work

--- src/SongCompiler.js
+++ src/SongCompiler.js
@@ -1,5 +1,5 @@
 // File name, if not specified in cli, 'FREEDOM_DiVE' is default.
-let name = process.argv[2] || 'FREEDOM_DiVE';
+let name = process.argv[2] || 'FREEDOM DiVE [FOUR DIMENSIONS]';
 
 // Where to find the .osu file
 let path = './songs/'+name+'.osu';
