6136c2967a9135042106dae5e804b6702aa1fcbe
Matias Vazquez-Levi <50473168+matiasvlevi@users.noreply.github.com>
2021-07-08 12:38:54 -0400

    underscores

--- src/SongCompiler.js
+++ src/SongCompiler.js
@@ -1,10 +1,10 @@
 // File name, if not specified in cli, 'FREEDOM DiVE [FOUR DIMENSIONS]' is default.
-let name = process.argv[2] || 'FREEDOM DiVE [FOUR DIMENSIONS]';
+let name = process.argv[2] || 'FREEDOM_DiVE_[FOUR_DIMENSIONS]';
 
 // Where to find the .osu file
 let path = './songs/'+name+'.osu';
 // Where to save the file once compiled into .json.
-let compiledPath = './compiled/'+name+'.json';
+let compiledPath = './webapp/compiled_songs/'+name+'.json';
 
 const osujson = require('osu-json');
 
