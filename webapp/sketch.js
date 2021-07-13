d47f9f1772ea8d666cb1d5bf2dc93b676d824945
Matias Vazquez-Levi <50473168+matiasvlevi@users.noreply.github.com>
2021-07-13 18:09:24 -0400

    added map_name value to get the name of the current map

--- webapp/sketch.js
+++ webapp/sketch.js
@@ -1,16 +1,18 @@
 
 
 let osu;
-
+let map_name;
 function preload() {
-  let osu_URL = "http://127.0.0.1:8080/compiled_songs/FREEDOM_DiVE_[FOUR DIMENSIONS].json";
+  let osu_URL = "http://127.0.0.1:8080/compiled_songs/FREEDOM_DiVE_[FOUR_DIMENSIONS].json";
   // http request osu map from gh repo
   fetch(osu_URL)
   .then(function (response) {
     return response.json();
   })
   .then(function (data) {
-    osu = data;
+    console.log(data)
+    osu = data.HitObjects;
+    map_name = data.General.AudioFilename.split('.')[0];
   })
   .catch(function (error) {
     console.log("Error: " + error);
@@ -50,39 +52,15 @@ function draw() {
     pop();
   } else {
     
-    
-
     background(51); 
     scale(2);
+
+    push();
     translate(margin/2, margin/2);
     stroke(0);
     noFill();
     rect(0, 0, playArea_X, playArea_Y);
-
-
-
-
-    if (time >= osu[osu.length-1].time - speed - 1) {
-      // If max time exceeded, restart at 0.
-      time = 0;
-    } else {
-      // Increment time value
-      time+=speed;
-    }
-    // Scan every hitobject & display at right time
-    for (let i = 0; i < osu.length; i++) {
-      if (osu[i].time >= time && osu[i].time <= (time + delay)) {
-        let type = osu[i].type.type;
-        if (type === 'circle') {
-          hitObject = new Circle(osu[i].x, osu[i].y, osu[i].time);
-        } else if (type === 'slider') {
-          hitObject = new Slider(osu[i].x, osu[i].y, osu[i].path, osu[i].time);
-        } else {
-          console.log('Unhandled type: ' + type + ' found at index: ' + i);
-          console.log('Here is the hitObject: ', osu[i]);
-        }
-        hitObject.render();
-      }
-    }
+    drawHitObjects();
+    pop();
   }
 }
