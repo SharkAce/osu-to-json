cf5e0d942db9223c1275fc0dd98f7362cb87db0d
Matias Vazquez-Levi <50473168+matiasvlevi@users.noreply.github.com>
2021-07-13 19:01:46 -0400

    slider class render bezier

--- webapp/classes/slider.js
+++ webapp/classes/slider.js
@@ -6,17 +6,27 @@ class Slider {
   }
   render() {
     noStroke();
-
+    textSize(8);
     fill(0, 255, 100);
-    text('Start point', this.pos.x, this.pos.y);
+    text('Start point', this.pos.x + 10, this.pos.y);
     ellipse(this.pos.x, this.pos.y, 4, 4);
 
     fill(0, 0, 255);
-    text('Bezier passthrough', this.passthrough.x, this.passthrough.y);
+    text('Bezier passthrough', this.passthrough.x + 10, this.passthrough.y);
     ellipse(this.passthrough.x, this.passthrough.y, 4, 4);
 
     fill(255, 0, 0);
-    text('End point', this.end.x, this.end.y);
+    text('End point', this.end.x + 10, this.end.y);
     ellipse(this.end.x, this.end.y, 4, 4);
+
+    // Render bezier curve
+    bezierCurve(
+      [this.pos.x, this.pos.y],
+      [this.passthrough.x, this.passthrough.y],
+      [this.end.x, this.end.y],
+      10
+    );
+
   }
+  //Add click event handling
 }
