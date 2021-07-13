e293bbd7a197bb976ea96b5ee1ef6ca410f8d7b9
Matias Vazquez-Levi <50473168+matiasvlevi@users.noreply.github.com>
2021-07-13 19:01:55 -0400

    display changes

--- webapp/classes/circle.js
+++ webapp/classes/circle.js
@@ -6,8 +6,9 @@ class Circle {
   }
   render() {
     noStroke();
+    textSize(8);
     fill(255);
-    text('circle', this.pos.x, this.pos.y)
+    text('circle', this.pos.x + 10, this.pos.y)
     ellipse(this.pos.x, this.pos.y, this.size, this.size);
   }
   // Add click event handling
