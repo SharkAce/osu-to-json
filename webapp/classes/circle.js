806703fa81357051e501732e8f7c86262f565c56
Matias Vazquez-Levi <50473168+matiasvlevi@users.noreply.github.com>
2021-07-13 19:21:00 -0400

    applied thickness & color

--- webapp/classes/circle.js
+++ webapp/classes/circle.js
@@ -2,13 +2,15 @@ class Circle {
   constructor(x, y, time) {
     this.time = time;
     this.pos = createVector(x, y);
-    this.size = 8;
+    this.size = 24;
+    this.color = [0,0,0,50];
   }
   render() {
     noStroke();
     textSize(8);
-    fill(255);
-    text('circle', this.pos.x + 10, this.pos.y)
+    fill(255)
+    text('circle', this.pos.x + 12, this.pos.y)
+    fill(this.color[0],this.color[1],this.color[2],this.color[3]);
     ellipse(this.pos.x, this.pos.y, this.size, this.size);
   }
   // Add click event handling
--- webapp/classes/slider.js
+++ webapp/classes/slider.js
@@ -3,30 +3,33 @@ class Slider {
     this.pos = createVector(x, y);
     this.end = createVector(path.end.x, path.end.y);
     this.passthrough = createVector(path.passthrough.x, path.passthrough.y);
+    this.color = [0,0,0,50];
   }
   render() {
-    noStroke();
-    textSize(8);
-    fill(0, 255, 100);
-    text('Start point', this.pos.x + 10, this.pos.y);
-    ellipse(this.pos.x, this.pos.y, 4, 4);
-
-    fill(0, 0, 255);
-    text('Bezier passthrough', this.passthrough.x + 10, this.passthrough.y);
-    ellipse(this.passthrough.x, this.passthrough.y, 4, 4);
-
-    fill(255, 0, 0);
-    text('End point', this.end.x + 10, this.end.y);
-    ellipse(this.end.x, this.end.y, 4, 4);
-
     // Render bezier curve
     bezierCurve(
       [this.pos.x, this.pos.y],
       [this.passthrough.x, this.passthrough.y],
       [this.end.x, this.end.y],
-      10
+      10,
+      24,
+      this.color
     );
 
+    // Render points
+    noStroke();
+    textSize(8);
+    fill(0, 255, 100);
+    text('Start point', this.pos.x + 10, this.pos.y);
+    ellipse(this.pos.x, this.pos.y, 2, 2);
+
+    fill(0, 0, 255);
+    text('Bezier passthrough', this.passthrough.x + 10, this.passthrough.y);
+    ellipse(this.passthrough.x, this.passthrough.y, 2, 2);
+
+    fill(255, 0, 0);
+    text('End point', this.end.x + 10, this.end.y);
+    ellipse(this.end.x, this.end.y, 2, 2);
   }
   //Add click event handling
 }
