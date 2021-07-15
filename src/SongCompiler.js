39bb3d06f2a7edd22ca06f6ee205d248ae7d843c
Matias Vazquez-Levi <50473168+matiasvlevi@users.noreply.github.com>
2021-07-15 02:55:54 -0400

    lint

--- src/SongCompiler.js
+++ src/SongCompiler.js
@@ -2,20 +2,20 @@
 let name = process.argv[2] || 'FREEDOM_DiVE_[FOUR_DIMENSIONS]';
 
 // Where to find the .osu file
-let path = './songs/'+name+'.osu';
+let path = './songs/' + name + '.osu';
 // Where to save the file once compiled into .json.
-let compiledPath = './webapp/compiled_songs/'+name+'.json';
+let compiledPath = './webapp/compiled_songs/' + name + '.json';
 
-const osujson = require('osu-json');
+const osujson = require('osu-parse');
 
 const fs = require('fs');
 let file = fs.readFileSync(path, 'utf8');
 
 
-osujson.ParseOSUFileAsync(file).then( (output)=>{
-	let data = JSON.stringify(output);
-	fs.writeFileSync(compiledPath, data)
-})
-.catch( (err)=>{
+osujson.ParseOSUFileAsync(file).then((output) => {
+    let data = JSON.stringify(output);
+    fs.writeFileSync(compiledPath, data)
+  })
+  .catch((err) => {
     throw new Error(err);
-});
+  });
